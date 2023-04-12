5a261aff0a57ec15e98ef28bc1314dcf34a94040
Blake Saari <78383023+blakesaari@users.noreply.github.com>
2023-04-11 19:35:46 -1000

    Update 6.sql

--- SQL/HW5_Travel_DB/6.sql
+++ SQL/HW5_Travel_DB/6.sql
@@ -4,8 +4,7 @@ SELECT
   Hotel.hotelName AS Hotel_Name,
   Room.type AS Type_of_Room,
   Room.price AS Price_of_Room
-FROM 
-  Booking 
+FROM Booking 
   JOIN Guest ON Booking.guestNo = Guest.guestNo 
   JOIN Room ON Booking.roomNo = Room.roomNo AND Booking.hotelNo = Room.hotelNo 
   JOIN Hotel ON Hotel.hotelNo = Hotel.hotelNo 
